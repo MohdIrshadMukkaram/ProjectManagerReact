9e07c873b08f66b5a848f4b554f1c828c54194da
Mohammed Irshad <46225985+MohdIrshadMukkaram@users.noreply.github.com>
2023-12-23 18:44:38 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import ProjectsSideBar from "./components/projectsSideBar";
+
+function App() {
+  return (
+    <main className="h-screen my-8">
+      <ProjectsSideBar/>
+    </main>
+  );
+}
+
+export default App;
